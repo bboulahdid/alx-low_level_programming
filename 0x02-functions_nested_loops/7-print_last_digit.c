0e4f13c2fa2a3e0f90a252aa0ea862576f6b0238
Boubker Boulahdid <boulahdid.boubker@gmail.com>
2023-03-21 18:59:57 -0700

    10-add.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,10 @@
  */
 int print_last_digit(int n)
 {
-	n = (n < 0) ? n * -1 : n;
+	int last_digit = n % 10;
 
-	_putchar('0' + (n % 10));
-
-	return (n % 10);
+	last_digit = (last_digit < 0) ? last_digit * -1 : last_digit;
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
 
