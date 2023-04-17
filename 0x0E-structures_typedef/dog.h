56ed49c8b58f35ec37252acac65521e2dbb6545e
Boubker Boulahdid <boulahdid.boubker@gmail.com>
2023-04-17 05:31:35 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's structure
+ * @name: the dogs's name
+ * @age: the dogs's age
+ * @owner: the owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
