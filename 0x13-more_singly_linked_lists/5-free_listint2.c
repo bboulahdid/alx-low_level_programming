92c7a3e1f773b1553e8e9ed2ae7bf9482ab1308b
Boubker Boulahdid <boulahdid.boubker@gmail.com>
2023-05-01 13:20:36 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: a pointer to the @ of the first node in the singly linked list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (*head != NULL)
+	{
+		tmp_node = (*head)->next;
+		free(*head);
+		*head = tmp_node;
+	}
+
+	head = NULL;
+}
+
